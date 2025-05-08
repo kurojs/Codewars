58d438abfa6e8792b90cade8e6005e2c7d0543dd
Kuro <kuuro.js@gmail.com>
2025-05-07 20:28:42 -0600

    fix file structure

--- Downloads/Codewars-main/Java/8 kyu Remove exclamation marks/Solution.java
+++
@@ -1,8 +0,0 @@
-// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
-
-// My solution
-class Solution {
-    static String removeExclamationMarks(String s) {
-      return s.replaceAll("!", "");
-    }
-}
