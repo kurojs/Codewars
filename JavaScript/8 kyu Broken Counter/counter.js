3a62b2e8b0bda5f7182e3ee18e02b149d544bfb7
Kuro クロ <kuuro.js@gmail.com>
2025-05-18 22:43:58 -0600

    Completed 5 Codewars 8 kyu JS challenges: - Fixed Counter prototype methods syntax errors - Created function to return weekday name from number input - Implemented personalized greeting based on name and owner - Defined age-based drink function according to given rules - Built function to remove trailing exclamation marks from strings

---
+++ JavaScript/8 kyu Broken Counter/counter.js
@@ -0,0 +1,35 @@
+/* Our counter prototype is broken. Can you spot, what's wrong here?
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype.increase() {
+  this.value++;
+};
+
+Counter.prototype.getValue() {
+  return this.value;
+};
+
+Counter.prototype.reset() {
+  this.value = 0;
+};
+*/
+
+// My solution
+
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype.increase = function () {
+  this.value++;
+};
+
+Counter.prototype.getValue = function () {
+  return this.value;
+};
+
+Counter.prototype.reset = function () {
+  this.value = 0;
+};
