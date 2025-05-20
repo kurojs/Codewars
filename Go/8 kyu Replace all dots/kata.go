6d8292222f372c3b4e3d3406d7bb3ff438778911
kurojs <kuuro.js@gmail.com>
2025-05-19 19:17:41 -0600

    Solve 5 katas: string repeat & dot fix in Go, vaporwave converter, stub cigarette math, and nice array logic in TypeScript

---
+++ Go/8 kyu Replace all dots/kata.go
@@ -0,0 +1,22 @@
+/*
+The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+But it's not working properly.
+Task
+
+Fix the bug so we can all go home early.
+Notes
+
+String str will never be null.
+*/
+
+// My solution
+package kata
+
+import (
+	"regexp"
+)
+
+func ReplaceDots(str string) string {
+	return regexp.MustCompile(`\.`).ReplaceAllString(str, "-")
+}
