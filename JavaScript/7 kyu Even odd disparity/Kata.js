7f07ce4551baca82f46b852b1dea1ba9154ddcd7
kurojs <kuuro.js@gmail.com>
2025-06-23 11:41:38 -0600

    feat(katas): add 5 JavaScript katas (7 kyu level)
    - JavaScript 7 kyu: Find Shortest Word Length - Return the length of the shortest word in a given string.
    - JavaScript 7 kyu: Magic Three - Check if any combination of three numbers in an array sums to 0.
    - JavaScript 7 kyu: Even vs Odd - Return the difference between even and odd number counts in an array.
    - JavaScript 7 kyu: Guitar Note - Determine the musical note at a specific fret and string on a guitar.
    - JavaScript 7 kyu: Credit Card Issuer - Identify the card issuer from a credit card number.

---
+++ JavaScript/7 kyu Even odd disparity/Kata.js
@@ -0,0 +1,34 @@
+/*
+  Given an array, return the difference between the count of even numbers and the count of odd numbers.
+  0 will be considered an even number.
+
+  For example:
+  solve([0,1,2,3]) = 0 because there are two even numbers and two odd numbers. 
+  Even - Odd = 2 - 2 = 0.  
+
+  Let's now add two letters to the last example:
+
+  solve([0,1,2,3,'a','b']) = 0. Again, Even - Odd = 2 - 2 = 0. Ignore letters. 
+
+  The input will be an array of lowercase letters and numbers only.
+
+  In some languages (Haskell, C++, and others), input will be an array of strings:
+
+  solve ["0","1","2","3","a","b"] = 0 
+
+  Good luck!
+*/
+
+// Solution
+
+function solve(a) {
+  let even = 0,
+    odd = 0;
+  for (let val of a) {
+    if (typeof val === "number") {
+      val % 2 === 0 ? even++ : odd++;
+    }
+  }
+
+  return even - odd;
+}
