4131fde2a4a2803d0a4d3efe870adf62d41697df
kurojs <kuuro.js@gmail.com>
2025-09-20 21:39:08 -0600

    feat(katas): add 5 new katas in TypeScript and JavaScript (8 kyu, 7 kyu)
    * TypeScript 8 kyu: Counting sheep - Count the number of `true` values
    in an array.
    * TypeScript 7 kyu: Test Your Knowledge Of Function Scope - Implement a
    curried function for addition.
    * JavaScript 7 kyu: Converting integer to currency format - Format
    integers with commas as thousands separators.
    * JavaScript 7 kyu: RaNDoM CAsE - Randomly change the case of each
    character in a string.
    * JavaScript 7 kyu: MinMaxMin Bounded Nums - Find smallest, largest, and
    smallest missing number in a range.

---
+++ TypeScript/7 kyu Test Your Knowledge Of Function Scope/Kata.ts
@@ -0,0 +1,12 @@
+/*
+Write a function that adds from two invocations.
+
+All inputs will be integers.
+
+add(3)(4)  // 7
+add(12)(20) // 32
+*/
+
+// Solution
+
+export const add = (x: number) => (y: number) => x + y;
