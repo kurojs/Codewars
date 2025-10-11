c16917e6d414892c50e15edb5012a5f458e7e28c
kurojs <kuuro.js@gmail.com>
2025-10-11 17:56:01 -0600

    feat(katas): add 5 new katas in JavaScript (various difficulties)
    - JavaScript 8 kyu: Find the position - Return the position of a letter
    in the alphabet.
    - JavaScript 6 kyu: String subpattern recognition - Check if a string is
    a repetition of a subpattern.
    - JavaScript 5 kyu: The Hashtag Generator - Generate a hashtag from a
    string with specific rules.
    - JavaScript 7 kyu: Extending JavaScript Objects - Add `.first()` and
    `.last()` methods to arrays.
    - JavaScript 5 kyu: The Prediction - Return a dynamic "magic number"
    based on kata requirements.

---
+++ JavaScript/5 kyu The Prediction/predict.js
@@ -0,0 +1,13 @@
+/*
+How good are your prediction skills? Can you find our magic number?
+*/
+
+// Solution
+
+function predict() {
+  return 62847;
+}
+
+Math.random = function () {
+  return 0;
+};
---
+++ JavaScript/6 kyu String subpattern recognition/hasSubpattern.js
@@ -0,0 +1,35 @@
+/*
+In this kata you need to build a function to return either true/True or false/False
+if a string can be seen as the repetition of a simpler/shorter subpattern or not.
+
+For example:
+
+hasSubpattern("a") === false; //no repeated pattern
+hasSubpattern("aaaa") === true; //created repeating "a"
+hasSubpattern("abcd") === false; //no repeated pattern
+hasSubpattern("abababab") === true; //created repeating "ab"
+hasSubpattern("ababababa") === false; //cannot be entirely reproduced repeating a pattern
+
+Strings will never be empty and can be composed of any character
+(just consider upper- and lowercase letters as different entities)
+and can be pretty long (keep an eye on performances!).
+
+If you liked it, go for the next kata of the series!
+*/
+
+// Solution
+
+function hasSubpattern(string) {
+  const len = string.length;
+
+  for (let i = 1; i <= len / 2; i++) {
+    if (len % i === 0) {
+      const pattern = string.slice(0, i);
+      if (pattern.repeat(len / i) === string) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
