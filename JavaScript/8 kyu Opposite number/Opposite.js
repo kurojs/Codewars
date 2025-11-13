bc1eadae01cebaebabb837a964bc7ddc03ef29df
kurojs <kuuro.js@gmail.com>
2025-11-13 17:42:33 -0600

    feat(katas): add 5 new katas in JavaScript (7-8 kyu)
    - JavaScript 7 kyu: Oh dear God Is it bugged - Checks if a string
    matches a specific date-time format.
    - JavaScript 7 kyu: Who ate the last cookie? - Returns who ate the last
    cookie based on input type.
    - JavaScript 7 kyu: Stanton measure - Calculates the Stanton measure
    from an array.
    - JavaScript 8 kyu: Swap Values - Swaps the two values in an array.
    - JavaScript 8 kyu: Opposite number - Returns the additive inverse of a
    number.

---
+++ JavaScript/8 kyu Opposite number/Opposite.js
@@ -0,0 +1,15 @@
+/*
+Very simple, given a number, find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+// Solution
+
+function opposite(number) {
+  return number * -1;
+}
