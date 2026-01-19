ec67a534ca5acc2843cafe03b5c2a1b7a1964a2e
クロ <kuuro.js@gmail.com>
2026-01-18 21:26:26 -0600

    feat: solve 7 kyu Incrementer, Unusual Five and Encrypt this katas

---
+++ JavaScript/7 kyu Unusual Five/unusualFive.js
@@ -0,0 +1,14 @@
+/*
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+*/
+
+function unusualFive() {
+  // Creamos una cadena de cinco caracteres. ¡Sencillo como un disparo láser!
+  return 'abcde'.length;
+  // El truco es que la propiedad 'length' de cualquier cadena nos da el número de caracteres,
+  // sin usar dígitos ni operadores prohibidos. ¡Elemental, mi querido Kuro!
+}
