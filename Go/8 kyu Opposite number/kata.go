4f205801ec76169893255053d7bf253fe47ef615
kurojs <kuuro.js@gmail.com>
2026-08-19 18:29:26 -0600

    feat(katas): add 5 new katas in Go, Php, Cpp, Python and Rust (6-8 kyu)

---
+++ Go/8 kyu Opposite number/kata.go
@@ -0,0 +1,15 @@
+/* Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+1: -1
+14: -14
+-34: 34
+*/
+
+// My solution
+
+package kata
+
+func Opposite(number int) int {
+	return -number
+}
---
+++ Php/6 kyu Duplicate Encoder/duplicateEncode.php
@@ -0,0 +1,23 @@
+/*
+The goal of this exercise is to convert a string to a new string where each character
+in the new string is "(" if that character appears only once in the original string,
+or ")" if that character appears more than once in the original string.
+Ignore capitalization when determining if a character is a duplicate.
+
+Examples:
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))(("
+*/
+
+// Solution
+
+function duplicateEncode(string $word): string {
+    $word = strtolower($word);
+    $result = '';
+    for ($i = 0; $i < strlen($word); $i++) {
+        $result .= substr_count($word, $word[$i]) > 1 ? ')' : '(';
+    }
+    return $result;
+}
